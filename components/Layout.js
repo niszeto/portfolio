f950b6ea164248237e12347ec5d61c3cf70bfe5f
niszeto <niszeto@gmail.com>
2019-02-03 12:47:30 -0800

    google analytics added

--- components/Layout.js
+++ components/Layout.js
@@ -1,105 +1,223 @@
 import Head from "next/head";
 
-const Layout = ({ children }) => {
-  return (
-    <div>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <title>Nicholas Szeto Portfolio</title>
-      </Head>
-      <main>{children}</main>
-      <style jsx global>{`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-          padding: 0;
-        }
+import { initGA, logPageView } from "../utils/analytics";
 
-        body {
-          background-image: linear-gradient(#8fc1e3, #f7f9fb);
-        }
+class Layout extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
 
-        main {
-          background-image: linear-gradient(
-              0deg,
-              transparent 24%,
-              #ffffff 25%,
-              #ffffff 26%,
-              transparent 27%,
-              transparent 74%,
-              #ffffff 75%,
-              #ffffff 76%,
-              transparent 77%,
-              transparent
-            ),
-            linear-gradient(
-              90deg,
-              transparent 24%,
-              #ffffff 25%,
-              #ffffff 26%,
-              transparent 27%,
-              transparent 74%,
-              #ffffff 75%,
-              #ffffff 76%,
-              transparent 77%,
-              transparent
-            );
-          background-size: 50px 50px;
-        }
+  render() {
+    const { children } = this.props;
 
-        a {
-          text-decoration: none;
-          color: black;
-        }
+    return (
+      <div>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <title>Nicholas Szeto Portfolio</title>
+        </Head>
+        <main>{children}</main>
+        <style jsx global>{`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+          }
 
-        .font {
-          font-family: Helvetica, sans-serif;
-        }
+          body {
+            background-image: linear-gradient(#8fc1e3, #f7f9fb);
+          }
 
-        .font-size {
-          font-size: 1.2em;
-        }
+          main {
+            background-image: linear-gradient(
+                0deg,
+                transparent 24%,
+                #ffffff 25%,
+                #ffffff 26%,
+                transparent 27%,
+                transparent 74%,
+                #ffffff 75%,
+                #ffffff 76%,
+                transparent 77%,
+                transparent
+              ),
+              linear-gradient(
+                90deg,
+                transparent 24%,
+                #ffffff 25%,
+                #ffffff 26%,
+                transparent 27%,
+                transparent 74%,
+                #ffffff 75%,
+                #ffffff 76%,
+                transparent 77%,
+                transparent
+              );
+            background-size: 50px 50px;
+          }
 
-        .font-color {
-          color: #687864;
-        }
+          a {
+            text-decoration: none;
+            color: black;
+          }
 
-        .header-font {
-          color: #31708e;
-        }
+          .font {
+            font-family: Helvetica, sans-serif;
+          }
 
-        .box-shadow {
-          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
-            0 6px 20px 0 rgba(0, 0, 0, 0.19);
-        }
+          .font-size {
+            font-size: 1.2em;
+          }
 
-        .indent {
-          text-indent: 10px;
-        }
+          .font-color {
+            color: #687864;
+          }
 
-        .margin-ten {
-          margin: 10px;
-        }
+          .header-font {
+            color: #31708e;
+          }
 
-        .margin-twenty {
-          margin: 20px;
-        }
+          .box-shadow {
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+              0 6px 20px 0 rgba(0, 0, 0, 0.19);
+          }
 
-        .padding-ten {
-          padding: 10px;
-        }
+          .indent {
+            text-indent: 10px;
+          }
 
-        .padding-twenty {
-          padding: 20px;
-        }
+          .margin-ten {
+            margin: 10px;
+          }
 
-        .rounded-corner {
-          border-radius: 5%;
-        }
-      `}</style>
-    </div>
-  );
-};
+          .margin-twenty {
+            margin: 20px;
+          }
+
+          .padding-ten {
+            padding: 10px;
+          }
+
+          .padding-twenty {
+            padding: 20px;
+          }
+
+          .rounded-corner {
+            border-radius: 5%;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
 
 export default Layout;
+
+// const Layout = ({ children }) => {
+// return (
+//   <div>
+//     <Head>
+//       <meta name="viewport" content="width=device-width, initial-scale=1" />
+//       <meta charSet="utf-8" />
+//       <title>Nicholas Szeto Portfolio</title>
+//     </Head>
+//     <main>{children}</main>
+//     <style jsx global>{`
+//       * {
+//         box-sizing: border-box;
+//         margin: 0;
+//         padding: 0;
+//       }
+
+//       body {
+//         background-image: linear-gradient(#8fc1e3, #f7f9fb);
+//       }
+
+//       main {
+//         background-image: linear-gradient(
+//             0deg,
+//             transparent 24%,
+//             #ffffff 25%,
+//             #ffffff 26%,
+//             transparent 27%,
+//             transparent 74%,
+//             #ffffff 75%,
+//             #ffffff 76%,
+//             transparent 77%,
+//             transparent
+//           ),
+//           linear-gradient(
+//             90deg,
+//             transparent 24%,
+//             #ffffff 25%,
+//             #ffffff 26%,
+//             transparent 27%,
+//             transparent 74%,
+//             #ffffff 75%,
+//             #ffffff 76%,
+//             transparent 77%,
+//             transparent
+//           );
+//         background-size: 50px 50px;
+//       }
+
+//       a {
+//         text-decoration: none;
+//         color: black;
+//       }
+
+//       .font {
+//         font-family: Helvetica, sans-serif;
+//       }
+
+//       .font-size {
+//         font-size: 1.2em;
+//       }
+
+//       .font-color {
+//         color: #687864;
+//       }
+
+//       .header-font {
+//         color: #31708e;
+//       }
+
+//       .box-shadow {
+//         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+//           0 6px 20px 0 rgba(0, 0, 0, 0.19);
+//       }
+
+//       .indent {
+//         text-indent: 10px;
+//       }
+
+//       .margin-ten {
+//         margin: 10px;
+//       }
+
+//       .margin-twenty {
+//         margin: 20px;
+//       }
+
+//       .padding-ten {
+//         padding: 10px;
+//       }
+
+//       .padding-twenty {
+//         padding: 20px;
+//       }
+
+//       .rounded-corner {
+//         border-radius: 5%;
+//       }
+//     `}</style>
+//   </div>
+// );
+// };
+
+// export default Layout;
